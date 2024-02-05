f01bf33998779057404b806cd13e2e48eaf3e096
LEE, TAEGON <taegon1998@gmail.com>
2024-02-05 22:51:37 +0900

    feat: CarImage entity 생성 및 속성 추가
    - ID, 차종, 대표 이미지, 생성 시간, 업데이트 시간 필드를 포함
    - ID 필드는 @GeneratedValue 어노테이션을 사용해 기본 키를 자동으로 관리

---
+++ back/reservation/src/main/java/com/genesisairport/reservation/entity/CarImage.java
@@ -0,0 +1,25 @@
+package com.genesisairport.reservation.entity;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "car_image")
+public class CarImage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "sell_name", nullable = false, length = 20)
+    private String sellName;
+
+    @Column(name = "image_url", nullable = false, length = 2048)
+    private String imageUrl;
+
+    @Column(name = "create_datetime", nullable = false)
+    private LocalDateTime createDatetime;
+
+    @Column(name = "update_datetime", nullable = false)
+    private LocalDateTime updateDatetime;
+}
