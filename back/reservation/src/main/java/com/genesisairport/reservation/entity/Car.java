98614676d6cd43570e51665ebd122370d6d61be0
LEE, TAEGON <taegon1998@gmail.com>
2024-02-05 22:43:03 +0900

    feat: Car entity 생성 및 속성 추가
    - ID, 차량 고유 식별자, 차종, 차량번호, 생성 시간, 업데이트 시간 필드를 포함
    - ID 필드는 @GeneratedValue 어노테이션을 사용해 기본 키를 자동으로 관리

---
+++ back/reservation/src/main/java/com/genesisairport/reservation/entity/Car.java
@@ -0,0 +1,28 @@
+package com.genesisairport.reservation.entity;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "car")
+public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "vin", length = 50)
+    private String vin;
+
+    @Column(name = "sell_name", length = 20)
+    private String sellName;
+
+    @Column(name = "plate_number", length = 20)
+    private String plateNumber;
+
+    @Column(name = "create_datetime", nullable = false)
+    private LocalDateTime createDatetime;
+
+    @Column(name = "update_datetime", nullable = false)
+    private LocalDateTime updateDatetime;
+}
