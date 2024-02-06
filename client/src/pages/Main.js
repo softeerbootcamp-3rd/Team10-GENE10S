f9b5e32ba6dfacd4e9d0299a3809e055645cb2d0
seonghwan <aiya0422@gmail.com>
2024-02-06 16:19:00 +0900

    feat: 배너 버튼 수정
    배너 버튼 부분을 옅은 검은색으로 수정

--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -9,18 +9,24 @@ export default function Main() {
     <>
       <Header />
       <div className={classNames('content')}>
-        <img src={homeBanner} alt="banner" />
-        <div className={classNames('banner-button')}>
-          <Link to="/bot" className={classNames('banner-button__button-left')}>
-            예약하기
-          </Link>
-          <Link
-            to="/reservation"
-            className={classNames('banner-button__button-right')}
-          >
-            예약봇
-          </Link>
+        <div className={classNames('banner')}>
+          <img src={homeBanner} alt="banner" />
+          <div className={classNames('banner-button-block')}>
+            <Link
+              to="/bot"
+              className={classNames('banner-button-block__button-left')}
+            >
+              예약하기
+            </Link>
+            <Link
+              to="/reservation"
+              className={classNames('banner-button-block__button-right')}
+            >
+              예약봇
+            </Link>
+          </div>
         </div>
+        <div>picture</div>
       </div>
       <Footer />
     </>
