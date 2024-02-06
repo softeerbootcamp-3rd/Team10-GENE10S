a9b384afac6728f6cc2c0a64013bdde812551b86
LEE, TAEGON <taegon1998@gmail.com>
2024-02-06 22:44:35 +0900

    feat: 요청 Dto 'UserProfile' 생성
    - 현대자동차 사용자 정보 조회 API를 사용에 필요한 token 값을 얻기 위해 sid 값을 요청에 포함

--- server/src/main/java/com/genesisairport/reservation/Request/LoginRequest.java
+++ server/src/main/java/com/genesisairport/reservation/Request/LoginRequest.java
@@ -1,16 +1,28 @@
 package com.genesisairport.reservation.Request;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 public class LoginRequest {
     @Getter
     @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
     public static class Login {
         private String grantType;
         private String code;
         private String redirectUri;
         private String clientId;
         private String clientSecret;
+    }
+
+    @Getter
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class UserProfile {
+        private String sid;
     }
 }
