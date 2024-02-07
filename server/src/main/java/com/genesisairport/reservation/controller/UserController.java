04ab740f31f8d6d89a88230acc994203d2fb0c09
insiderhj <insiderhj@gmail.com>
2024-02-07 10:51:54 +0900

    feat: user profile API GetMapping 내용 수정
    GetMapping에 "/profile" 링크를 안 달아놨었는데 추가했습니다

--- server/src/main/java/com/genesisairport/reservation/controller/UserController.java
+++ server/src/main/java/com/genesisairport/reservation/controller/UserController.java
@@ -9,10 +9,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/user")
+@RequestMapping("/v1/user")
 public class UserController {
 
-    @GetMapping
+    @GetMapping("/profile")
     public ResponseEntity<DataResponseDto<UserResponse.Profile>> getResult() {
         UserResponse.Profile result = UserResponse.Profile.builder()
                 .name("김희진")
