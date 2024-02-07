416904651baef7d59acffbcedbae089bb4a5fb20
h-sooah <s2sooas2@ewhain.net>
2024-02-07 11:05:10 +0900

    feat : car list dummy API 추
    GET /v1/reservation/car-list 더미용 API 추가

---
+++ server/src/main/java/com/genesisairport/reservation/controller/ReservationController.java
@@ -0,0 +1,30 @@
+package com.genesisairport.reservation.controller;
+
+import com.genesisairport.reservation.service.ReservationService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@CrossOrigin
+@Slf4j
+@RequestMapping("/v1/reservation")
+public class ReservationController {
+
+    private final ReservationService reservationService;
+
+    @GetMapping("/car-list")
+    public ResponseEntity getCarList() {
+        return new ResponseEntity(
+                reservationService.getCarList(),
+                HttpStatus.OK
+        );
+    }
+
+}
---
+++ server/src/main/java/com/genesisairport/reservation/service/ReservationService.java
@@ -0,0 +1,36 @@
+package com.genesisairport.reservation.service;
+
+import com.genesisairport.reservation.Response.ReservationResponse;
+import com.genesisairport.reservation.common.DataResponseDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class ReservationService {
+
+    public DataResponseDto<List<ReservationResponse.CarInfo>> getCarList() {
+        List<ReservationResponse.CarInfo> carInfoList = new ArrayList<>();
+
+
+        ReservationResponse.CarInfo carInfo = ReservationResponse.CarInfo.builder()
+                .sellName("Sonata")
+                .plateNumber("222라 2222")
+                .build();
+        ReservationResponse.CarInfo carInfo1 = ReservationResponse.CarInfo.builder()
+                .sellName("Sonata")
+                .plateNumber("111마 1111")
+                .build();
+
+        carInfoList.add(carInfo);
+        carInfoList.add(carInfo1);
+
+        return DataResponseDto.of(carInfoList);
+    }
+
+}
