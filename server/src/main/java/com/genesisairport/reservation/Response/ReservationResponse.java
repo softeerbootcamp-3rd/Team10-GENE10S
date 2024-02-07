414d4cbf9039a1e98feb8a6306ac88d7333b802b
insiderhj <insiderhj@gmail.com>
2024-02-07 17:39:52 +0900

    refactor: Cookie에 해당하는 user 반환하는 메서드 개발
    HttpServletRequest로부터 Cookie 정보를 읽어 그에 해당하는 user를 Optional에 담아 반환하는 메서드 개발

--- server/src/main/java/com/genesisairport/reservation/Response/ReservationResponse.java
+++ server/src/main/java/com/genesisairport/reservation/Response/ReservationResponse.java
@@ -12,4 +12,5 @@ public class ReservationResponse {
         private String plateNumber;
     }
 
+
 }
---
+++ server/src/main/java/com/genesisairport/reservation/service/SessionService.java
@@ -0,0 +1,41 @@
+package com.genesisairport.reservation.service;
+
+import com.genesisairport.reservation.entity.Customer;
+import com.genesisairport.reservation.entity.Session;
+import com.genesisairport.reservation.respository.CustomerRepository;
+import com.genesisairport.reservation.respository.SessionRepository;
+import lombok.RequiredArgsConstructor;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class SessionService {
+
+    private final SessionRepository sessionRepository;
+    private final CustomerRepository customerRepository;
+
+    public Optional<Customer> getLoggedInCustomer(HttpServletRequest request) {
+        if (request == null)
+            return Optional.empty();
+
+        Cookie[] cookies = request.getCookies();
+        if (cookies == null)
+            return Optional.empty();
+
+        for (Cookie cookie : cookies) {
+            if ("sid".equals(cookie.getName())) {
+                String sid = cookie.getValue();
+                Optional<Session> session = sessionRepository.findBySessionId(sid);
+                if (session.isPresent()) {
+                    Long customerId = session.get().getCustomerId();
+                    return customerRepository.findById(customerId);
+                }
+            }
+        }
+        return Optional.empty();
+    }
+}
