678b4001e88fc21846abc13d8ad7216f5f4d3156
seonghwan <aiya0422@gmail.com>
2024-02-09 02:25:52 +0900

    feat: 날짜 선택에서 예약 정보 선택으로 이동하는 기능 추가
    - 선택한 값들을 상위 모달 컴포넌트로 전달할 수 있도록 수정
    - 더미 데이터 추가
    - 데이터의 유효성을 검사하는 코드 추가
    - modal_body의 height 주석처리

--- client/src/components/ModalDate.js
+++ client/src/components/ModalDate.js
@@ -1,8 +1,20 @@
 import classNames from 'classnames';
 
-export default function ModalDate() {
-  const date = '2024-02-01';
-  const time = '8:00';
+export default function ModalDate({ setCurrentStep, props }) {
+  // const departureDate = props.departureDate;
+  // const departureTime = props.departureTime;
+  // const pickupDate = props.pickupDate;
+  // const pickupTime = props.pickupTime;
+  const departureDate = '2024-02-01';
+  const departureTime = '8:00';
+  const pickupDate = '2024-02-12';
+  const pickupTime = '9:00';
+  console.log(departureDate === '');
+
+  function handleNext() {
+    if (departureDate === '' || departureTime === '' || pickupDate === '' || pickupTime === '') return;
+    setCurrentStep(departureDate, departureTime, pickupDate, pickupTime);
+  }
 
   return (
     <div className={classNames('modal_body')}>
@@ -13,13 +25,13 @@ export default function ModalDate() {
       <div className={classNames('frame_right')}>
         <div className={classNames('category_row')}>
           <span>1. 센터 방문</span>
-          <span>{`${date} / ${time}`}</span>
+          <span>{departureDate === '' ? '센터 방문 시간을 선택해주세요' : `${departureDate} / ${departureTime}`}</span>
         </div>
         <div className={classNames('category_row')}>
           <span>2. 픽업 시간</span>
-          <span>픽업 시간을 선택해주세요</span>
+          <span>{pickupDate === '' ? '픽업 시간을 선택해주세요' : `${pickupDate} / ${pickupTime}`}</span>
         </div>
-        <div className={classNames('button')}>
+        <div className={classNames('button')} onClick={handleNext}>
           <span>다음</span>
         </div>
       </div>
--- client/src/pages/ReservationModal.js
+++ client/src/pages/ReservationModal.js
@@ -1,15 +1,33 @@
 import classNames from 'classnames';
 import ModalHeader from '../components/ModalHeader';
 import ModalDate from '../components/ModalDate';
+import { useState } from 'react';
 
 export default function ReservationModal() {
   const shopName = '블루핸즈 인천공항점';
-  let currentStep = 'date';
+  const [currentStep, setCurrentStep] = useState('date');
+  const [departureDate, setDepartureDate] = useState('');
+  const [departureTime, setDepartureTime] = useState('');
+  const [pickupDate, setPickupDate] = useState('');
+  const [pickupTime, setPickupTime] = useState('');
+
+  function handleDate(departureDate, departureTime, pickupDate, pickupTime) {
+    console.log(departureDate, departureTime, pickupDate, pickupTime);
+    setDepartureDate(departureDate);
+    setDepartureTime(departureTime);
+    setPickupDate(pickupDate);
+    setPickupTime(pickupTime);
+    setCurrentStep('info');
+  }
+
+  const dateProps = { departureDate, departureTime, pickupDate, pickupTime };
 
   return (
     <>
       <ModalHeader shopName={shopName} currentStep={currentStep} />
-      <div className={classNames('reservation-modal')}>{currentStep === 'date' ? <ModalDate /> : null}</div>
+      <div className={classNames('reservation-modal')}>
+        {currentStep === 'date' ? <ModalDate setCurrentStep={handleDate} props={dateProps} /> : null}
+      </div>
     </>
   );
 }
