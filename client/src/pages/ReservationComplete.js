24c4c163c4d658067d18b7fe748fb3e75498127c
seonghwan <aiya0422@gmail.com>
2024-02-08 13:56:19 +0900

    fix: 선택한 서비스가 보이도록 수정
    - 예약화면 스타일 적용
    - 클래스명 오류 수정

--- client/src/pages/ReservationComplete.js
+++ client/src/pages/ReservationComplete.js
@@ -84,11 +84,11 @@ export default function ReservationComplete() {
           <div className={classNames('row--title')}>선택 서비스</div>
           <div className={classNames('row__content')}>
             <div className={classNames('content__line')}>
-              <span className={classNames({ selsected: reservInfo ? reservInfo.serviceList.includes('oil') : false })}>
+              <span className={classNames({ selected: reservInfo ? reservInfo.serviceList.includes('oil') : false })}>
                 각종 오일 및 호스 상태
               </span>
               <span
-                className={classNames({ selsected: reservInfo ? reservInfo.serviceList.includes('battery') : false })}
+                className={classNames({ selected: reservInfo ? reservInfo.serviceList.includes('battery') : false })}
               >
                 배터리 상태 및 충전 전압
               </span>
@@ -96,21 +96,21 @@ export default function ReservationComplete() {
             <div className={classNames('content__line')}>
               <span
                 className={classNames({
-                  selsected: reservInfo ? reservInfo.serviceList.includes('engine-run') : false,
+                  selected: reservInfo ? reservInfo.serviceList.includes('engine-run') : false,
                 })}
               >
                 엔진 구동 상태
               </span>
               <span
                 className={classNames({
-                  selsected: reservInfo ? reservInfo.serviceList.includes('engine-cooler') : false,
+                  selected: reservInfo ? reservInfo.serviceList.includes('engine-cooler') : false,
                 })}
               >
                 엔진 냉각수
               </span>
               <span
                 className={classNames({
-                  selsected: reservInfo ? reservInfo.serviceList.includes('air-cleaner') : false,
+                  selected: reservInfo ? reservInfo.serviceList.includes('air-cleaner') : false,
                 })}
               >
                 에어클리너 점검
@@ -118,58 +118,56 @@ export default function ReservationComplete() {
             </div>
             <div className={classNames('content__line')}>
               <span
-                className={classNames({ selsected: reservInfo ? reservInfo.serviceList.includes('bottom') : false })}
+                className={classNames({ selected: reservInfo ? reservInfo.serviceList.includes('bottom') : false })}
               >
                 하체충격 및 손상 여부
               </span>
               <span
-                className={classNames({ selsected: reservInfo ? reservInfo.serviceList.includes('breakpad') : false })}
+                className={classNames({ selected: reservInfo ? reservInfo.serviceList.includes('breakpad') : false })}
               >
                 브레이크패드 및 라이닝 마모
               </span>
             </div>
             <div className={classNames('content__line')}>
-              <span className={classNames({ selsected: reservInfo ? reservInfo.serviceList.includes('lamp') : false })}>
+              <span className={classNames({ selected: reservInfo ? reservInfo.serviceList.includes('lamp') : false })}>
                 각종 등화 장치 점검
               </span>
               <span
                 className={classNames({
-                  selsected: reservInfo ? reservInfo.serviceList.includes('engine-mount') : false,
+                  selected: reservInfo ? reservInfo.serviceList.includes('engine-mount') : false,
                 })}
               >
                 엔진 미션 마운트
               </span>
               <span
                 className={classNames({
-                  selsected: reservInfo ? reservInfo.serviceList.includes('suspension') : false,
+                  selected: reservInfo ? reservInfo.serviceList.includes('suspension') : false,
                 })}
               >
                 서스펜션 뉴계토우 점검
               </span>
             </div>
             <div className={classNames('content__line')}>
-              <span
-                className={classNames({ selsected: reservInfo ? reservInfo.serviceList.includes('shaft') : false })}
-              >
+              <span className={classNames({ selected: reservInfo ? reservInfo.serviceList.includes('shaft') : false })}>
                 스테빌라이저 및 드라이버 샤프트
               </span>
               <span
-                className={classNames({ selsected: reservInfo ? reservInfo.serviceList.includes('scanner') : false })}
+                className={classNames({ selected: reservInfo ? reservInfo.serviceList.includes('scanner') : false })}
               >
                 스캐너 고장코드 진단
               </span>
             </div>
             <div className={classNames('content__line')}>
               <span
-                className={classNames({ selsected: reservInfo ? reservInfo.serviceList.includes('heater') : false })}
+                className={classNames({ selected: reservInfo ? reservInfo.serviceList.includes('heater') : false })}
               >
                 에어컨 및 히터 작동 생태
               </span>
-              <span className={classNames({ selsected: reservInfo ? reservInfo.serviceList.includes('tire') : false })}>
+              <span className={classNames({ selected: reservInfo ? reservInfo.serviceList.includes('tire') : false })}>
                 타이어 공기압 및 마모도
               </span>
               <span
-                className={classNames({ selsected: reservInfo ? reservInfo.serviceList.includes('filter') : false })}
+                className={classNames({ selected: reservInfo ? reservInfo.serviceList.includes('filter') : false })}
               >
                 에어컨 필터
               </span>
