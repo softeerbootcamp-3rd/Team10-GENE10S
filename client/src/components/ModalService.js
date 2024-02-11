5514ab3f4e088c0eab1d06700e40be95f25c79c2
seonghwan <aiya0422@gmail.com>
2024-02-11 13:43:43 +0900

    feat: 예약 완료 기능을 제외한 서비스 선택 페이지 구현
    - 샘플 이미지 추가
    - 다른 페이지로 이동해도 입력값이 유지되도록 구현

---
+++ client/src/components/ModalService.js
@@ -0,0 +1,183 @@
+import classNames from 'classnames';
+import { BtnBlack } from './Button';
+import { useState } from 'react';
+
+export default function ModalService({ props, prevStep }) {
+  const [services, setServices] = useState(props.services);
+  const [request, setRequest] = useState(props.request);
+  const [coupon, setCoupon] = useState(props.coupon);
+
+  function handleServiceClick(event) {
+    const service = event.currentTarget.id;
+    if (services.includes(service)) {
+      console.log(service + ' remove');
+      setServices(services.filter(item => item !== service));
+    } else {
+      console.log(service + ' add');
+      setServices([...services, service]);
+    }
+    console.log(services);
+  }
+
+  function handlePrev() {
+    prevStep(services, request, coupon);
+  }
+
+  return (
+    <>
+      <div className={classNames('body')}>
+        <div className={classNames('frame_left')}>
+          <img className={classNames('image')} src={require(`../assets/sample-service.png`)} alt="" key="image" />
+        </div>
+        <div className={classNames('frame_right')}>
+          <div className={classNames('category_row')}>
+            <span className={classNames('title')}>5. 서비스선택</span>
+            <div className={classNames('content')}>
+              <div
+                id="oil"
+                className={classNames('select', { active: services.includes('oil') })}
+                onClick={e => {
+                  handleServiceClick(e);
+                  console.log(services);
+                }}
+              >
+                각종 오일 및 호스 상태
+              </div>
+              <div
+                id="battery"
+                className={classNames('select', { active: services.includes('battery') })}
+                onClick={e => handleServiceClick(e)}
+              >
+                배터리 상태 및 충전 전압
+              </div>
+              <div
+                id="engine-run"
+                className={classNames('select', { active: services.includes('engine-run') })}
+                onClick={e => handleServiceClick(e)}
+              >
+                엔진 구동 상태
+              </div>
+              <div
+                id="engine-cooler"
+                className={classNames('select', { active: services.includes('engine-cooler') })}
+                onClick={e => handleServiceClick(e)}
+              >
+                엔진 냉각수
+              </div>
+              <div
+                id="air-cleaner"
+                className={classNames('select', { active: services.includes('air-cleaner') })}
+                onClick={e => handleServiceClick(e)}
+              >
+                에어클리너
+              </div>
+              <div
+                id="bottom"
+                className={classNames('select', { active: services.includes('bottom') })}
+                onClick={e => handleServiceClick(e)}
+              >
+                하체충격 및 손상 여부
+              </div>
+              <div
+                id="breakpad"
+                className={classNames('select', { active: services.includes('breakpad') })}
+                onClick={e => handleServiceClick(e)}
+              >
+                브레이크 패드 및 라이닝 마모
+              </div>
+              <div
+                id="lamp"
+                className={classNames('select', { active: services.includes('lamp') })}
+                onClick={e => handleServiceClick(e)}
+              >
+                각종 등화 장치 점검
+              </div>
+              <div
+                id="engine-mount"
+                className={classNames('select', { active: services.includes('engine-mount') })}
+                onClick={e => handleServiceClick(e)}
+              >
+                엔진 미션 마운트
+              </div>
+              <div
+                id="suspension"
+                className={classNames('select', { active: services.includes('suspension') })}
+                onClick={e => handleServiceClick(e)}
+              >
+                서스펜션 뉴계토우 점검
+              </div>
+              <div
+                id="shaft"
+                className={classNames('select', { active: services.includes('shaft') })}
+                onClick={e => handleServiceClick(e)}
+              >
+                스테빌라이저 및 드라이버 샤프트
+              </div>
+              <div
+                id="scanner"
+                className={classNames('select', { active: services.includes('scanner') })}
+                onClick={e => handleServiceClick(e)}
+              >
+                스캐너 고장코드 진단
+              </div>
+              <div
+                id="heater"
+                className={classNames('select', { active: services.includes('heater') })}
+                onClick={e => handleServiceClick(e)}
+              >
+                에어컨 및 히터 작동 생태
+              </div>
+              <div
+                id="tire"
+                className={classNames('select', { active: services.includes('tire') })}
+                onClick={e => handleServiceClick(e)}
+              >
+                타이어 공기압 및 마모드
+              </div>
+              <div
+                id="filter"
+                className={classNames('select', { active: services.includes('filter') })}
+                onClick={e => handleServiceClick(e)}
+              >
+                에어컨 필터
+              </div>
+            </div>
+          </div>
+          <div className={classNames('category_row')}>
+            <span className={classNames('title')}>6. 추가 요청 사항</span>
+            <div className={classNames('content')}>
+              <input
+                type="text"
+                value={request}
+                className={classNames('input-left', 'w-max', 'input-text')}
+                onChange={e => {
+                  setRequest(e.target.value);
+                }}
+              />
+            </div>
+          </div>
+          <div className={classNames('category_row')}>
+            <span className={classNames('title')}>7. 쿠폰번호 입력</span>
+            <div className={classNames('content')}>
+              <input
+                type="text"
+                value={coupon}
+                className={classNames('input-left', 'w-400', 'input-text')}
+                onChange={e => {
+                  setCoupon(e.target.value);
+                }}
+              />
+              <div className={classNames('btn-check')}>
+                <span className={classNames('btn-text')}>쿠폰번호 확인하기</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={classNames('btn-group')}>
+        <BtnBlack text="이전" onClick={handlePrev} />
+        <BtnBlack text="예약하기" />
+      </div>
+    </>
+  );
+}
--- client/src/pages/ReservationModal.js
+++ client/src/pages/ReservationModal.js
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import ModalHeader from '../components/ModalHeader';
 import ModalDate from '../components/ModalDate';
 import ModalInfo from '../components/ModalInfo';
+import ModalService from '../components/ModalService';
 
 export default function ReservationModal() {
   const shopName = '블루핸즈 인천공항점';
@@ -16,9 +17,13 @@ export default function ReservationModal() {
   const [phone3, setPhone3] = useState('');
   const [sellName, setSellName] = useState('');
   const [plateNumber, setPlateNumber] = useState('');
+  const [services, setServices] = useState([]);
+  const [request, setRequest] = useState('');
+  const [coupon, setCoupon] = useState('');
 
   const dateProps = { departureDate, departureTime, pickupDate, pickupTime };
   const infoProps = { phone1, phone2, phone3, sellName, plateNumber };
+  const serviceProps = { services, request, coupon };
 
   function saveInfo(phone1, phone2, phone3, sellName, plateNumber) {
     setPhone1(phone1);
@@ -46,15 +51,20 @@ export default function ReservationModal() {
     setCurrentStep('service');
   }
 
+  function serviceToInfo(services, request, coupon) {
+    setServices(services);
+    setRequest(request);
+    setCoupon(coupon);
+    setCurrentStep('info');
+  }
+
   return (
     <div className={classNames('reservation-modal')}>
       <ModalHeader shopName={shopName} currentStep={currentStep} />
       <div className={classNames('modal-page')}>
         {currentStep === 'date' ? <ModalDate props={dateProps} nextStep={dateToInfo} /> : null}
         {currentStep === 'info' ? <ModalInfo props={infoProps} prevStep={infoToDate} nextStep={infoToService} /> : null}
-        {currentStep === 'service' ? (
-          <ModalInfo props={infoProps} prevStep={infoToDate} nextStep={infoToService} />
-        ) : null}
+        {currentStep === 'service' ? <ModalService props={serviceProps} prevStep={serviceToInfo} /> : null}
       </div>
     </div>
   );
