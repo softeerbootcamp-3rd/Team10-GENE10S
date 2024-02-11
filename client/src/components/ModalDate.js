094077997f7a1f19773718ace1fca64eadd2206c
seonghwan <aiya0422@gmail.com>
2024-02-11 10:50:22 +0900

    feat: 달력 기능을 제외한 날짜 선택 페이지 구현
    - 달력 기능은 정보 입력, 서비스 선택 기능 구현 후 작업 예정
    - 이전 버튼 클릭시 메인 화면으로 이동하도록 구현
    - 다음 버튼 클릭시 입력값을 저장하는 함수 호출

--- client/src/components/ModalDate.js
+++ client/src/components/ModalDate.js
@@ -1,8 +1,9 @@
 import classNames from 'classnames';
 import { BtnBlack } from './Button';
 import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
-export default function ModalDate({ prevStep, nextStep, props }) {
+export default function ModalDate({ nextStep, props }) {
   const [departureDate, setDepartureDate] = useState(props.departureDate);
   const [departureTime, setDepartureTime] = useState(props.departureTime);
   const [pickupDate, setPickupDate] = useState(props.pickupDate);
@@ -21,44 +22,44 @@ export default function ModalDate({ prevStep, nextStep, props }) {
     nextStep(departureDate, departureTime, pickupDate, pickupTime);
   }
 
-  function handlePrev() {
-    prevStep();
-  }
-
   return (
-    <div className={classNames('body')}>
-      <div className={classNames('frame_left')}>
-        <div className={classNames('calender')} />
-        <div className={classNames('available-times')} />
-      </div>
-      <div className={classNames('frame_right')}>
-        <div className={classNames('category_row')}>
-          <span className={classNames('title')}>1. 센터 방문</span>
-          <span className={classNames('content')}>
-            <div className={classNames('input-area', 'w-200')}>
-              <span className={classNames('input-text')}>{departureDate}</span>
-            </div>
-            <div className={classNames('input-area', 'w-160')}>
-              <span className={classNames('input-text')}>{departureTime}</span>
-            </div>
-          </span>
+    <>
+      <div className={classNames('body')}>
+        <div className={classNames('frame_left')}>
+          <div className={classNames('calender')} />
+          <div className={classNames('available-times')} />
         </div>
-        <div className={classNames('category_row')}>
-          <span className={classNames('title')}>2. 픽업 시간</span>
-          <span className={classNames('content')}>
-            <div className={classNames('input-area', 'w-200')}>
-              <span className={classNames('input-text')}>{pickupDate}</span>
-            </div>
-            <div className={classNames('input-area', 'w-160')}>
-              <span className={classNames('input-text')}>{pickupTime}</span>
-            </div>
-          </span>
-        </div>
-        <div className={classNames('btn-group')}>
-          <BtnBlack text="이전" onClick={handlePrev} />
-          <BtnBlack text="다음" onClick={handleNext} />
+        <div className={classNames('frame_right')}>
+          <div className={classNames('category_row')}>
+            <span className={classNames('title')}>1. 센터 방문</span>
+            <span className={classNames('content')}>
+              <div className={classNames('input-area', 'w-200')}>
+                <span className={classNames('input-text')}>{departureDate}</span>
+              </div>
+              <div className={classNames('input-area', 'w-160')}>
+                <span className={classNames('input-text')}>{departureTime}</span>
+              </div>
+            </span>
+          </div>
+          <div className={classNames('category_row')}>
+            <span className={classNames('title')}>2. 픽업 시간</span>
+            <span className={classNames('content')}>
+              <div className={classNames('input-area', 'w-200')}>
+                <span className={classNames('input-text')}>{pickupDate}</span>
+              </div>
+              <div className={classNames('input-area', 'w-160')}>
+                <span className={classNames('input-text')}>{pickupTime}</span>
+              </div>
+            </span>
+          </div>
         </div>
       </div>
-    </div>
+      <div className={classNames('btn-group')}>
+        <Link to="/" replace={true}>
+          <BtnBlack text="이전" />
+        </Link>
+        <BtnBlack text="다음" onClick={handleNext} />
+      </div>
+    </>
   );
 }
