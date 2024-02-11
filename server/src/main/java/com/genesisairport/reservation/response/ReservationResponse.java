a87b5aa88b84881e9dbd1349fa9086f7cf36231d
admin <rlawnghks99@gmail.com>
2024-02-11 22:04:48 +0900

    refactor: ReservationResponse로 dto 병합
    기존에는 예약의 날짜, 리스트, 정보 저장에 대한 response dto를 각각 생성함
    
    동일 카테고리의 dto를 하나로 묶어서 유지보수를 용이하게함

--- server/src/main/java/com/genesisairport/reservation/response/ReservationResponse.java
+++ server/src/main/java/com/genesisairport/reservation/response/ReservationResponse.java
@@ -42,4 +42,36 @@ public class ReservationResponse {
         private Boolean available;
     }
 
+    @Getter
+    @Builder
+    public static class ReservationPostResponse {
+        private boolean reservationStatus;
+        private String repairShopAddress;
+        private String customerName;
+    }
+
+    @Getter
+    @Builder
+    public static class ReservationListAbstract{
+        private long reservationId;
+        private String departureTime;
+        private String arrivalTime;
+        private String progressStage;
+        private String carSellName;
+        private String repairShop;
+
+        // 기본 생성자를 추가합니다.
+        public ReservationListAbstract() {
+        }
+
+        // 이 생성자를 사용하여 객체를 생성할 수 있도록 합니다.
+        public ReservationListAbstract(long reservationId, String departureTime, String arrivalTime, String progressStage, String carSellName, String repairShop) {
+            this.reservationId = reservationId;
+            this.departureTime = departureTime;
+            this.arrivalTime = arrivalTime;
+            this.progressStage = progressStage;
+            this.carSellName = carSellName;
+            this.repairShop = repairShop;
+        }
+    }
 }
