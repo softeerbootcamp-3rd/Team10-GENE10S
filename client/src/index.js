3a8381e6331e27a76d7694a81621d86c49f68801
h-sooah <s2sooas2@ewhain.net>
2024-02-12 21:43:01 +0900

    feat : 예약 인트로 페이지 퍼블리싱 완료

--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,7 @@ import './styles/styles.scss';
 import Main from './pages/Main';
 import NotFound from './pages/NotFound';
 import Reservation from './pages/Reservation';
+import ReservationIntro from './pages/ReservationIntro';
 import Bot from './pages/Bot';
 import Mypage from './pages/Mypage';
 import ReservationComplete from './pages/ReservationComplete';
@@ -15,6 +16,7 @@ root.render(
     <Routes>
       <Route path="/" element={<Main />} />
       <Route path="/reservation" element={<Reservation />} />
+      <Route path="/reservation-intro" element={<ReservationIntro />} />
       <Route path="/reservation/complete" element={<ReservationComplete />} />
       <Route path="/bot" element={<Bot />} />
       <Route path="/mypage" element={<Mypage />} />
