6328314f887ecac9b27f230cc9e6122965c04956
seonghwan <aiya0422@gmail.com>
2024-02-12 23:10:28 +0900

    refactor: 모달 헤더에 svg를 화살표 컴포넌트로 변경
    유지보수성을 높이기 위해 컴포넌트로 만든 화살표를 사용하도록 수정

--- client/src/components/ModalHeader.js
+++ client/src/components/ModalHeader.js
@@ -1,54 +1,32 @@
 import classNames from 'classnames';
+import { ProgressArrow200, ProgressArrow500 } from './Arrow';
 
-export default function ModalHeader() {
-
+export default function ModalHeader({ shopName, currentStep }) {
   return (
     <div className={classNames('modal-header')}>
       <div className={classNames('shop-name')}>
-        <span className={classNames('text')}>
-          블루핸즈 인천공항점
-        </span>
+        <span className={classNames('text')}>{shopName}</span>
         <div className={classNames('dropdown')}>
-          <div className={classNames('progress-arrow-200')}>
-            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="12" viewBox="0 0 20 12" fill="none">
-              <path d="M1.5 1.66406L10.4048 10.3375L19 1.66406" stroke="#DDD8D2" strokeWidth="2"/>
-            </svg>
-          </div>
+          <ProgressArrow200 />
         </div>
       </div>
 
       <div className={classNames('steps')}>
-        <div className={classNames('menu', 'active')}>
-          <span className={classNames('text')}>
-            날짜 선택
-          </span>
+        <div className={classNames('menu', { active: currentStep === 'date' })}>
+          <span className={classNames('text')}>날짜 선택</span>
         </div>
-        <div className={classNames('progress-arrow-500')}>
-          <svg xmlns="http://www.w3.org/2000/svg" width="12" height="20" viewBox="0 0 12 20" fill="none">
-            <path d="M1 18.75L9.67346 9.84524L0.999999 1.25" stroke="#726E69" strokeWidth="2"/>
-          </svg>
+        <ProgressArrow500 />
+        <div className={classNames('menu', { active: currentStep === 'info' })}>
+          <span className={classNames('text')}>예약 정보</span>
         </div>
-        <div className={classNames('menu')}>
-          <span className={classNames('text')}>
-            예약 정보
-          </span>
-        </div>
-        <div className={classNames('progress-arrow-500')}>
-          <svg xmlns="http://www.w3.org/2000/svg" width="12" height="20" viewBox="0 0 12 20" fill="none">
-            <path d="M1 18.75L9.67346 9.84524L0.999999 1.25" stroke="#726E69" strokeWidth="2"/>
-          </svg>
-        </div>
-        <div className={classNames('menu')}>
-          <span className={classNames('text')}>
-            서비스 선택
-          </span>
+        <ProgressArrow500 />
+        <div className={classNames('menu', { active: currentStep === 'service' })}>
+          <span className={classNames('text')}>서비스 선택</span>
         </div>
       </div>
 
       <div className={classNames('exit')}>
-        <span className={classNames('text')}>
-          X
-        </span>
+        <span className={classNames('text')}>X</span>
       </div>
     </div>
   );
