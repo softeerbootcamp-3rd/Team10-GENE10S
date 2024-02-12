b2f9f8a1be1f04a56213f1c9cb01ba6da5dd4529
insiderhj <insiderhj@gmail.com>
2024-02-12 15:43:59 +0900

    refactor: modalPage.scss 구조 변경
    기존 nested 구조에서 '>'를 활용한 구조로 변경

--- client/src/pages/ReservationDate.js
+++ client/src/pages/ReservationDate.js
@@ -1,5 +1,5 @@
 import classNames from 'classnames';
-import Calendar from '../components/calendar';
+import Calendar from '../components/Calendar';
 import ModalHeader from '../components/ModalHeader';
 
 export default function ReservationDate() {
@@ -7,7 +7,7 @@ export default function ReservationDate() {
   return (
     <div className={classNames('modal-page')}>
       <ModalHeader />
-      <div className={classNames('modal-body')}>
+      <div className={classNames('body')}>
         <div className={classNames('frame-left')}>
           <Calendar year={2024} month={3} />
         </div>
