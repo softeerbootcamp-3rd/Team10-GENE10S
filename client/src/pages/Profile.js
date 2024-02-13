7858a968b385ff2820d60ee0658d7fb850978fc0
h-sooah <s2sooas2@ewhain.net>
2024-02-13 19:32:13 +0900

    refactor : 내 정보 수정 API 연결
    프로필 페이지에서 업데이트 할 정보 입력 후 '저장하기' 버튼을 누르면, '내 정보 수정' API를 호출하고, 성공하면 mypage로 redirect 처리

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -4,12 +4,51 @@ import Header from '../components/Header';
 import Modal from '../components/Modal';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+import { redirect } from 'react-router-dom';
 
 export default function Profile() {
 
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [profileInfo, setProfileInfo] = useState({});
   const [carList, setCarList] = useState([]);
+
+  const [userName, setUserName] = useState('');
+  const [birthdate, setBirthdate] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
+
+  const handleUserNameChange = (event) => {
+    setUserName(event.target.value);
+  };
+
+  const handleBirthdateChange = (event) => {
+    setBirthdate(event.target.value);
+  };
+
+  const handlePhoneNumberChange = (event) => {
+    setPhoneNumber(event.target.value);
+  };
+
+  const handleUpdateClick = () => {
+    const requestBody = {
+      name : userName,
+      birthdate : birthdate,
+      phoneNumber : phoneNumber
+    };
+
+    try {
+      const response = axios.patch('v1/user/info', requestBody, {
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+
+      window.location.href = '/mypage'; 
+
+    } catch (error) {
+      console.error('Error updating user information:', error);
+    }
+
+  };
 
   useEffect(() => {
     axios.get('v1/user/info')
@@ -48,7 +87,7 @@ export default function Profile() {
         <div className={classNames('profile-info')}>
           <div className={classNames('profile-row')}>
             <span className={classNames('key')}>이름</span>
-            <input className={classNames('input')} defaultValue={profileInfo.name}></input>
+            <input className={classNames('input')} defaultValue={profileInfo.name} value={userName} onChange={handleUserNameChange} />
           </div>
           <div className={classNames('profile-row')}>
             <span className={classNames('key')}>이메일</span>
@@ -56,11 +95,11 @@ export default function Profile() {
           </div>
           <div className={classNames('profile-row')}>
             <span className={classNames('key')}>생년월일</span>
-            <input className={classNames('input')} defaultValue={profileInfo.birthdate}></input>
+            <input className={classNames('input')} defaultValue={profileInfo.birthdate} value={birthdate} onChange={handleBirthdateChange} />
           </div>
           <div className={classNames('profile-row')}>
             <span className={classNames('key')}>연락처</span>
-            <input className={classNames('input')} defaultValue={profileInfo.phoneNumber}></input>
+            <input className={classNames('input')} defaultValue={profileInfo.phoneNumber} value={phoneNumber} onChange={handlePhoneNumberChange} />
           </div>
           <div className={classNames('profile-row')}>
             <span className={classNames('key')}>보유 차량</span>
@@ -76,13 +115,13 @@ export default function Profile() {
           <div className={classNames('btn-white')}>
             <span className={classNames('text')}>취소하기</span>
           </div>
-          <div className={classNames('btn-orange')}>
+          <div className={classNames('btn-orange')} onClick={handleUpdateClick}>
             <span className={classNames('text')}>저장하기</span>
           </div>
         </div>
       </div>
       <Footer />
-
+      
       {isModalOpen && <Modal onClose={closeModal}/>}
     </div>
   );
