4834e9e1420495cfe24bed6f8021bdaaccf4113d
insiderhj <insiderhj@gmail.com>
2024-02-13 11:27:30 +0900

    feat: GET user/info API 개발완료
    customer, car entity로부터 데이터 전달. LocalDate는 CommonDateFormat 클래스를 만들어서 String으로 변환해줌

--- server/src/main/java/com/genesisairport/reservation/response/UserResponse.java
+++ server/src/main/java/com/genesisairport/reservation/response/UserResponse.java
@@ -1,6 +1,9 @@
 package com.genesisairport.reservation.response;
 
+import com.genesisairport.reservation.entity.Car;
 import lombok.*;
+
+import java.util.List;
 
 public class UserResponse {
 
@@ -13,8 +16,29 @@ public class UserResponse {
     @Getter
     @Setter
     @Builder
-    @AllArgsConstructor
-    @NoArgsConstructor
+    public static class UserInfo {
+        private Long userId;
+        private String name;
+        private String email;
+        private String birthdate;
+        private String phoneNumber;
+        private List<CarInfo> carList;
+
+        @Getter
+        @Setter
+        @Builder
+        public static class CarInfo {
+            private Long carId;
+            private String vin;
+            private String sellName;
+            private String plateNumber;
+            private String imageUrl;
+        }
+    }
+
+    @Getter
+    @Setter
+    @Builder
     public static class Profile {
         private String name;
         private String email;
