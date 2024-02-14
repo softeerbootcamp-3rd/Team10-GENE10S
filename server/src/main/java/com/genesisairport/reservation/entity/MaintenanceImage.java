15fdfe3911f4de97e7b7945f4eba904a0a5099c1
seonghwan <aiya0422@gmail.com>
2024-02-15 02:14:13 +0900

    feat: 예약 내역 상세 조회 response dto 추가
    - ResponseDetail dto 추가
    - 이미지의 status와 url 값을 활용하기 위해 해당 필드에 Getter 추가
    - 예약 id를 통해 해당 예약 및 연관 정보를 얻고 dto로 만드는 메서드 추가

--- server/src/main/java/com/genesisairport/reservation/entity/MaintenanceImage.java
+++ server/src/main/java/com/genesisairport/reservation/entity/MaintenanceImage.java
@@ -3,6 +3,7 @@ package com.genesisairport.reservation.entity;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
@@ -19,9 +20,11 @@ public class MaintenanceImage {
     private Long id;
 
     @Column(nullable = false)
+    @Getter
     private Integer status;
 
     @Column(name = "image_url", nullable = false, length = 2048)
+    @Getter
     private String imageUrl;
 
     @Column(name = "create_datetime", nullable = false)
