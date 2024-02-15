e34ef327f27ed5359e9df34c63fdf4d061c71ace
seonghwan <aiya0422@gmail.com>
2024-02-15 15:13:18 +0900

    feat: AnimatedComponent 스크롤 감지 방식 수정
    overflow 방식으로 통일시키기 위해 window 스크롤 감지 -> 내부 스크롤 감지 방식으로 수정

--- client/src/components/AnimatedComponent.js
+++ client/src/components/AnimatedComponent.js
@@ -1,56 +1,63 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { motion } from 'framer-motion';
-import { useState, useEffect } from "react";
+import { useState, useEffect } from 'react';
 
-const AnimatedComponent = ({ children, name = "animation",
-  delay = 0.3, duration = 0.2, yDuration = 0.8, scale = 1, translateY = 50, triggerPosition = 0.5 }) => {
+const AnimatedComponent = ({
+  children,
+  name = 'animation',
+  delay = 0.3,
+  duration = 0.2,
+  yDuration = 0.8,
+  scale = 1,
+  translateY = 50,
+  triggerPosition = 0.8,
+}) => {
   const [isVisible, setIsVisible] = useState(false);
+  const componentPosition = useRef();
 
   useEffect(() => {
-    const handleScroll = () => {
-      const scrollPosition = window.scrollY + window.innerHeight * triggerPosition;
-      const componentPosition = document
-        .querySelector("." + name)
-        .getBoundingClientRect().top + window.scrollY;
+    const page = document.querySelector('#root').querySelector('div');
+    const position = document.querySelector(`.${name}`).getBoundingClientRect().top;
+    componentPosition.current = position;
 
-      if (scrollPosition > componentPosition && !isVisible) {
+    const handleScroll = () => {
+      const scrollPosition = page.scrollTop + window.innerHeight * triggerPosition;
+
+      if (scrollPosition > componentPosition.current && !isVisible) {
         setIsVisible(true);
       }
     };
 
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, [isVisible, triggerPosition]);
+    page.addEventListener('scroll', handleScroll);
+  }, []);
 
   const containerStyle = {
     opacity: 0,
-    y: translateY
+    y: translateY,
   };
 
   const animationStyle = {
     opacity: 1,
     y: 0,
-    transition: { delay }
+    transition: { delay },
   };
 
   return (
     <motion.div
-      className={ name }
+      className={name}
       initial={containerStyle}
-      animate={ isVisible ? animationStyle : {} }
-      whileHover={{ scale }} 
+      animate={isVisible ? animationStyle : {}}
+      whileHover={{ scale }}
       transition={{
-        ease: "easeInOut",
-        duration : { duration },
-        y: { duration : { yDuration } }
+        ease: 'easeInOut',
+        duration: { duration },
+        y: { duration: { yDuration } },
       }}
-      style={{ width: "100%" }}
+      style={{ width: '100%' }}
     >
       {children}
     </motion.div>
   );
-}
+};
 
 export default AnimatedComponent;
