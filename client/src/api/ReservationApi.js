4cdd070f9cd70e7cf642628c82062872bd40012f
insiderhj <insiderhj@gmail.com>
2024-02-15 15:26:05 +0900

    feat: 날짜 클릭 시 이용가능 시간 API 호출
    기존 더미데이터로 처리하던 부분을 실제 API로 처리하도록 변경했습니다.

--- client/src/api/ReservationApi.js
+++ client/src/api/ReservationApi.js
@@ -1,5 +1,26 @@
 import axios from 'axios';
 
+const formatDate = (date) => {
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0'); // 월은 0부터 시작하므로 +1 해줌
+  const day = String(date.getDate()).padStart(2, '0');
+
+  return `${year}-${month}-${day}`;
+}
+
+export async function getAvailableTime(year, month, day) {
+  try {
+    const response = await axios.get('/v1/reservation/time', {
+      params: { repairShop: '블루핸즈 인천공항점',
+                date: formatDate(new Date(year, month, day)) }
+    })
+
+    return response.data.data;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
 export async function getCarList() {
   try {
     const response = await axios.get('/v1/reservation/car-list');
