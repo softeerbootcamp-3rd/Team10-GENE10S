9171d05d0265d03bd6eae0afc86075e2ad4e645f
admin <rlawnghks99@gmail.com>
2024-02-15 10:49:46 +0900

    chore: console.log 제거

--- client/src/components/reservation_detail/ReservationDetailResult.js
+++ client/src/components/reservation_detail/ReservationDetailResult.js
@@ -39,9 +39,6 @@ export default function ReservationDetailResult() {
 
       setCumBeforeSizes(cumulativeBeforeWidths);
       setCumAfterSizes(cumulativeAfterWidths);
-
-      console.log(cumulativeBeforeWidths);
-      console.log(cumulativeAfterWidths);
     };
 
     loadImageWidths();
