186357d819752a976b4ff40865175030040f03e5
LEE, TAEGON <taegon1998@gmail.com>
2024-02-17 14:12:26 +0900

    chore: Axios baseURL 요청 경로 및 proxy 경로 변경
    - Axios baseURL: localhost:3000 -> S3 Bucket 경로
    - proxy: localhost:8080 -> EC2 서버 경로

--- client/src/api/Settings.js
+++ client/src/api/Settings.js
@@ -1,7 +1,7 @@
 import Axios from 'axios';
 
 const axios = Axios.create({
-  baseURL: 'http://localhost:8080/',
+  baseURL: 'http://genesis-airport-react-app-user.s3-website.ap-northeast-2.amazonaws.com/',
 });
 
 export default axios;
