988248cba0d9ccbc431dcc94a10bebf08fc5daec
LEE, TAEGON <102223636+taegon98@users.noreply.github.com>
2024-02-18 17:29:22 +0900

    remove: application.log 파일 생성 스크립트 삭제
    - 사용 되지 않는 application.log 파일 삭제

--- server/script/deploy.sh
+++ server/script/deploy.sh
@@ -1,26 +1,22 @@
 source /env.sh
 PROJECT_ROOT="/home/ubuntu/app/server"
-JAR_FILE="$PROJECT_ROOT/reservation-0.0.1-SNAPSHOT.jar"
 
-APP_LOG="$PROJECT_ROOT/application.log"
 ERROR_LOG="$PROJECT_ROOT/error.log"
 DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
 
 TIME_NOW=$(date +%c)
 
-echo "$TIME_NOW > $JAR_FILE 파일 복사" >> "$DEPLOY_LOG"
-cp "$PROJECT_ROOT/build/libs/$JAR_NAME" "$PROJECT_ROOT/"
-
-echo "$TIME_NOW > $JAR_FILE 파일 실행" >> "$DEPLOY_LOG"
-
-DEPLOY_PATH="$PROJECT_ROOT"
-BUILD_JAR="$DEPLOY_PATH/build/libs/$JAR_NAME"
+BUILD_JAR="$PROJECT_ROOT/build/libs/reservation-0.0.1-SNAPSHOT.jar"
 JAR_NAME=$(basename "$BUILD_JAR")
+
+echo "$TIME_NOW > $JAR_NAME 파일 복사" >> "$DEPLOY_LOG"
+cp "$BUILD_JAR" "$PROJECT_ROOT/"
 
 echo "> 현재 실행중인 애플리케이션 pid 확인" >> "$DEPLOY_LOG"
 CURRENT_PID=$(pgrep -f "$JAR_NAME")
 
-if [ -z "$CURRENT_PID" ]; then
+if [ -z "$CURRENT_PID" ]
+then
   echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> "$DEPLOY_LOG"
 else
   echo "> kill -15 $CURRENT_PID" >> "$DEPLOY_LOG"
@@ -28,6 +24,8 @@ else
   sleep 5
 fi
 
-DEPLOY_JAR="$DEPLOY_PATH/$JAR_NAME"
-echo "> DEPLOY_JAR 배포" >> "$DEPLOY_LOG"
+DEPLOY_JAR="$PROJECT_ROOT/$JAR_NAME"
+
+echo "$TIME_NOW > $DEPLOY_JAR 파일 실행" >> "$DEPLOY_LOG"
 nohup java -DDB_USERNAME="$DB_USERNAME" -DDB_PASSWORD="$DB_PASSWORD" -DREDIS_PASSWORD="$REDIS_PASSWORD" -jar "$DEPLOY_JAR" >> "$DEPLOY_LOG" 2>>"$ERROR_LOG" &
+echo "$TIME_NOW > $DEPLOY_JAR 파일 실행 완료" >> "$DEPLOY_LOG"
