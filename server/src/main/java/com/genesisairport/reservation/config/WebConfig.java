9c8914be498fbac63d45e478da2a83a908817f44
h-sooah <s2sooas2@ewhain.net>
2024-02-18 17:39:26 +0900

    hotfix : CORS error

---
+++ server/src/main/java/com/genesisairport/reservation/config/WebConfig.java
@@ -0,0 +1,23 @@
+package com.genesisairport.reservation.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000", "http://api.reservation.genesis-airport.com")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+
+
+}
