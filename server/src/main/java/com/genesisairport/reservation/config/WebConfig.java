6eac5d4c7164a655c9046817b625e3b14b204ce7
h-sooah <s2sooas2@ewhain.net>
2024-02-18 16:46:34 +0900

    hotfix : CORS error

--- server/src/main/java/com/genesisairport/reservation/config/WebConfig.java
+++ server/src/main/java/com/genesisairport/reservation/config/WebConfig.java
@@ -1,23 +1,30 @@
 package com.genesisairport.reservation.config;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @Order(Ordered.HIGHEST_PRECEDENCE)
-public class WebConfig implements WebMvcConfigurer {
+public class WebConfig {
+    @Bean
+    public FilterRegistrationBean<CorsFilter> corsFilterRegistrationBean() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("http://localhost:3000");
+        config.addAllowedOrigin("http://reservation.genesis-airport.com");
+        config.addAllowedMethod("*");
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry
-                .addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
-                .allowedOrigins("http://reservation.genesis-airport.com")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
-                .allowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean<CorsFilter> filterBean = new FilterRegistrationBean<>(new CorsFilter(source));
+        filterBean.setOrder(0);
+        return filterBean;
     }
 
 }
