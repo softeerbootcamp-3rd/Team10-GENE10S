d06c0a692c93836f1a86df7be3039d5a015ffe1f
h-sooah <s2sooas2@ewhain.net>
2024-02-18 17:03:01 +0900

    hotfix : CORS error

--- client/src/api/Settings.js
+++ client/src/api/Settings.js
@@ -4,8 +4,7 @@ const axios = Axios.create({
   baseURL: `${process.env.REACT_APP_SERVER_URL}`,
   withCredentials: true,
   headers: {
-    'Access-Control-Allow-Origin': `${process.env.REACT_APP_REDIRECT_URI}`,
-    'Access-Control-Allow-Credentials': "true"
+    'Access-Control-Allow-Origin': `${process.env.REACT_APP_REDIRECT_URI}`
   }
 });
 
