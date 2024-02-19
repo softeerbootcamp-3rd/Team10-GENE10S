b0d6ebea40ddb2d419c0630d35762a2de2cdc734
h-sooah <s2sooas2@ewhain.net>
2024-02-19 13:31:25 +0900

    feat : 특정 URI에 대해 SessionInterceptor 적용
    WebConfig에 addInterceptors() 함수 추가

--- server/src/main/java/com/genesisairport/reservation/config/WebConfig.java
+++ server/src/main/java/com/genesisairport/reservation/config/WebConfig.java
@@ -1,16 +1,23 @@
 package com.genesisairport.reservation.config;
 
 
+import com.genesisairport.reservation.common.SessionInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @Configuration
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class WebConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
@@ -22,4 +29,11 @@ public class WebConfig implements WebMvcConfigurer {
                 .maxAge(3600);
     }
 
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new SessionInterceptor(redisTemplate))
+                .order(1)
+                .addPathPatterns("/v1/**") //TODO v2 api 완성되면 추가
+                .excludePathPatterns("/v1/login", "/");
+    }
 }
