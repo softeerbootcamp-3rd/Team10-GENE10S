6ceb64454e91e7772a2a05a4ae09886ad3e0dcc9
admin <rlawnghks99@gmail.com>
2024-02-19 17:41:27 +0900

    feat: StepRepository에 커스텀 메서드 추가
    예약 id에 해당하는 특정 단계의 Step 엔티티를 반환함

--- server/src/main/java/com/genesisairport/reservation/respository/StepRepository.java
+++ server/src/main/java/com/genesisairport/reservation/respository/StepRepository.java
@@ -2,6 +2,13 @@ package com.genesisairport.reservation.respository;
 
 import com.genesisairport.reservation.entity.Step;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface StepRepository extends JpaRepository<Step, Long> {
+
+    Step findStepByReservationIdAndStage(
+            @Param("reservationId") Long reservation,
+            @Param("stage") String progress);
 }
