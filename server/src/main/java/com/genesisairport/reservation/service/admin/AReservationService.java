b04d9653b6edc77f40577ece244ab77896d95d2e
LEE, TAEGON <taegon1998@gmail.com>
2024-02-20 11:29:54 +0900

    feat: requestBody 조건에 맞는 예약 전체 내역을 반환하는 서비스 계층
    - ReservationRepository를 호출하여 requestBody에 맞는 전체 예약 내역을 반환

---
+++ server/src/main/java/com/genesisairport/reservation/service/admin/AReservationService.java
@@ -0,0 +1,21 @@
+package com.genesisairport.reservation.service.admin;
+
+import com.genesisairport.reservation.request.AdminRequest;
+import com.genesisairport.reservation.response.AdminResponse;
+import com.genesisairport.reservation.respository.ReservationRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class AReservationService {
+    private final ReservationRepository reservationRepository;
+
+    public List<AdminResponse.ReservationDetail> getAllReservations(AdminRequest.ReservationDetail requestBody) {
+        return reservationRepository.findReservations(requestBody);
+    }
+}
