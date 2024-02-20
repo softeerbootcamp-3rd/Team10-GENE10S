2017d3b83a57416f6463510bf03c899db45528e6
LEE, TAEGON <taegon1998@gmail.com>
2024-02-20 11:25:06 +0900

    feat: QueryDSL을 사용하여 예약 목록 조회 기능 구현
    - getBuilderForWhereClause: 매개변수로 입력받은 필드들에 값이 있으면(검색 조건이 걸려있으면), where 절에 포함시키고 나머지 경우는 건너뛴다.
    - getOrderBySpecifier: 매개변수로 정렬 컬럼 및 정렬 방식을 받고, OrderSpecifier 객체를 생성해서 반환

---
+++ server/src/main/java/com/genesisairport/reservation/respository/ReservationRepositoryImpl.java
@@ -0,0 +1,133 @@
+package com.genesisairport.reservation.respository;
+
+import com.genesisairport.reservation.common.CommonDateFormat;
+import com.genesisairport.reservation.request.AdminRequest;
+import com.genesisairport.reservation.response.AdminResponse;
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.Tuple;
+import com.querydsl.core.types.OrderSpecifier;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.logging.log4j.util.Strings;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.genesisairport.reservation.entity.QRepairShop.repairShop;
+import static com.genesisairport.reservation.entity.QReservation.reservation;
+import static com.genesisairport.reservation.entity.QCustomer.customer;
+import static com.genesisairport.reservation.entity.QStep.step;
+
+@RequiredArgsConstructor
+@Slf4j
+public class ReservationRepositoryImpl implements ReservationRepositoryCustom {
+
+    private final JPAQueryFactory jpaQueryFactory;
+
+    @Override
+    public List<AdminResponse.ReservationDetail> findReservations(AdminRequest.ReservationDetail reservationDetail) {
+
+        BooleanBuilder builderForWhereClause = getBuilderForWhereClause(
+                reservationDetail.getShopName(),
+                reservationDetail.getStartPickupDateTime(),
+                reservationDetail.getEndPickupDateTime(),
+                reservationDetail.getStartReturnDateTime(),
+                reservationDetail.getEndReturnDateTime(),
+                reservationDetail.getCustomerName(),
+                reservationDetail.getSellName(),
+                reservationDetail.getStage()
+        );
+
+        OrderSpecifier<?> orderBySpecifier = getOrderBySpecifier(
+                reservationDetail.getSortColumn(),
+                reservationDetail.getSortDirection()
+        );
+
+        List<Tuple> tuples = jpaQueryFactory
+                .select(
+                        reservation.id,
+                        repairShop.shopName,
+                        customer.name,
+                        reservation.sellName,
+                        reservation.departureTime,
+                        reservation.arrivalTime,
+                        step.stage
+                )
+                .from(reservation)
+                .innerJoin(customer).on(reservation.customer.eq(customer))
+                .innerJoin(repairShop).on(reservation.repairShop.eq(repairShop))
+                .innerJoin(step).on(step.reservation.eq(reservation))
+                .where(builderForWhereClause)
+                .orderBy(orderBySpecifier)
+                .fetch();
+        return convertToReservationDetailAdmin(tuples);
+    }
+
+    private List<AdminResponse.ReservationDetail> convertToReservationDetailAdmin(List<Tuple> tuples) {
+        List<AdminResponse.ReservationDetail> reservationDetailAdmins = new ArrayList<>();
+        for (Tuple t : tuples) {
+            AdminResponse.ReservationDetail reservationDetailAdmin = AdminResponse.ReservationDetail.builder()
+                    .reservationId(t.get(0, Long.class))
+                    .shopName(t.get(1, String.class))
+                    .customerName(t.get(2, String.class))
+                    .sellName(t.get(3, String.class))
+                    .departureTime(CommonDateFormat.datetime(t.get(4, LocalDateTime.class)))
+                    .arrivalTime(CommonDateFormat.datetime(t.get(5, LocalDateTime.class)))
+                    .stage(t.get(6, String.class))
+                    .build();
+            reservationDetailAdmins.add(reservationDetailAdmin);
+        }
+        return reservationDetailAdmins;
+    }
+
+    private BooleanBuilder getBuilderForWhereClause(String shopName, String startPickupDate, String endPickupDate, String startReturnDate, String endReturnDate, String customerName, String sellName, String stage) {
+        BooleanBuilder builderForWhereClause = new BooleanBuilder();
+
+        if (!Strings.isEmpty(shopName)) { // null 이거나 "" 가 아니면
+            builderForWhereClause.and(repairShop.shopName.eq(shopName));
+        }
+        if (!Strings.isEmpty(startPickupDate) && !Strings.isEmpty(endPickupDate)) {
+            builderForWhereClause.and(reservation.departureTime.between(CommonDateFormat.datetime(startPickupDate), CommonDateFormat.datetime(endPickupDate)));
+        }
+        if (!Strings.isEmpty(startReturnDate) && !Strings.isEmpty(endReturnDate)) {
+            builderForWhereClause.and(reservation.arrivalTime.between(CommonDateFormat.datetime(startReturnDate), CommonDateFormat.datetime(endReturnDate)));
+        }
+        if (!Strings.isEmpty(customerName)) {
+            builderForWhereClause.and(customer.name.eq(customerName));
+        }
+        if (!Strings.isEmpty(sellName)) {
+            builderForWhereClause.and(reservation.sellName.eq(sellName));
+        }
+        if (!Strings.isEmpty(stage)) {
+            builderForWhereClause.and(step.stage.eq(stage));
+        }
+
+        return builderForWhereClause;
+    }
+
+    private OrderSpecifier<?> getOrderBySpecifier(String sortColumn, String sortDirection) {
+        if (Strings.isEmpty(sortColumn)) {
+            return reservation.id.asc();
+        }
+
+        OrderSpecifier<?> orderBySpecifier;
+
+        switch (sortColumn) {
+            case "id":
+                orderBySpecifier = sortDirection.equals("asc") ? reservation.id.asc() : reservation.id.desc();
+                break;
+            case "departureTime":
+                orderBySpecifier = sortDirection.equals("asc") ? reservation.departureTime.asc() : reservation.departureTime.desc();
+                break;
+            case "arrivalTime":
+                orderBySpecifier = sortDirection.equals("asc") ? reservation.arrivalTime.asc() : reservation.arrivalTime.desc();
+                break;
+            default:
+                orderBySpecifier = reservation.id.asc();
+                break;
+        }
+        return orderBySpecifier;
+    }
+}
