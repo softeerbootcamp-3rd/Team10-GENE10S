68b69e2e5e1ac5eae2c5df719443d5783c2e3c98
LEE, TAEGON <taegon1998@gmail.com>
2024-02-21 10:42:55 +0900

    Merge branch 'develop' into feat/#233

--- admin/src/index.js
+++ admin/src/index.js
@@ -6,6 +6,8 @@ import Main from "./pages/Main";
 import NotFound from "./pages/NotFound";
 import { RecoilRoot } from "recoil";
 import TestPage from "./pages/TestPage";
+import ReservationDetail from "./pages/RservationDetail";
+import ReservationList from "./pages/ReservationList";
 import Account from "./pages/Account";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -15,6 +17,8 @@ root.render(
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/test" element={<TestPage />} />
+        <Route path="reservation" element={<ReservationList />} />
+        <Route path="/reservation/detail" element={<ReservationDetail />} />
         <Route path="*" element={<NotFound />} />
         <Route path="/admin/account" element={<Account />} />
       </Routes>
