5816f7a63c70a2c2360bdda25e571812c0b10404
LEE, TAEGON <taegon1998@gmail.com>
2024-02-22 01:48:04 +0900

    feat: 페이지 변경에 따른 Api 요청 로직 작성
    - onChange 이벤트를 활용해서 현재 페이지가 변경될 때마다, 변경된 페이지에 대한 관리자 계정 리스트를 갖고 오는 Api를 요청

--- admin/src/components/Pagination.js
+++ admin/src/components/Pagination.js
@@ -1,33 +1,45 @@
 import classNames from "classnames";
 import { Link } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
-export default function Pagination({ PageInfo, paginationSize }) {
-  let pageNumber = PageInfo.page;
+export default function Pagination({
+  page,
+  totalPages,
+  paginationSize,
+  onChange,
+}) {
+  let pageNumber = page;
   let startPage =
     Math.floor((pageNumber - 1) / paginationSize) * paginationSize + 1;
   let endPage = startPage + paginationSize - 1;
+
+  const location = useLocation();
+  const searchParams = new URLSearchParams(location.search);
 
   return (
     <>
       <div className={classNames("left-arrow")}>
         {startPage > 1 && (
-          <Link to={`?page=${startPage - 1}`} state={{ page: startPage - 1 }}>
+          <Link
+            to={`${location.pathname}?${searchParams.toString()}`}
+            state={{ page: startPage - 1 }}
+          >
             &lt;
           </Link>
         )}
       </div>
       <div className={classNames("page-number")}>
         {Array.from(
-          { length: Math.min(PageInfo.totalPages, endPage) - startPage + 1 },
+          { length: Math.min(totalPages, endPage) - startPage + 1 },
           (_, index) => startPage + index
-        ).map((pageNumber) => {
-          const isActive = PageInfo.page === pageNumber;
-
+        ).map((pageNumber, index) => {
           return (
             <Link
               to={`?page=${pageNumber}`}
               state={{ page: pageNumber }}
-              className={classNames("page-item", { active: isActive })}
+              className={classNames("page-item")}
+              key={index}
+              onClick={() => onChange(pageNumber)}
             >
               {pageNumber}
             </Link>
@@ -35,7 +47,7 @@ export default function Pagination({ PageInfo, paginationSize }) {
         })}
       </div>
       <div className={classNames("right-arrow")}>
-        {endPage < PageInfo.totalPages && (
+        {endPage < totalPages && (
           <Link to={`?page=${endPage + 1}`} state={{ page: endPage + 1 }}>
             &gt;
           </Link>
