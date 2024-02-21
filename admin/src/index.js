ff31d52050c188a5adab3b0cff31819885104bed
LEE, TAEGON <taegon1998@gmail.com>
2024-02-21 10:04:51 +0900

    feat: 계정 관리 페이지 컴포넌트 추가
    - 관리자가 계정을 관리할 수 있는 관리자 계정 관리 페이지로 이동할 수 있도록 Account component 추가

--- admin/src/index.js
+++ admin/src/index.js
@@ -1,20 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './styles/styles.scss';
-import Main from './pages/Main';
-import NotFound from './pages/NotFound';
-import { RecoilRoot } from 'recoil';
-import TestPage from './pages/TestPage';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./styles/styles.scss";
+import Main from "./pages/Main";
+import NotFound from "./pages/NotFound";
+import { RecoilRoot } from "recoil";
+import TestPage from "./pages/TestPage";
+import Account from "./pages/Account";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <RecoilRoot>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Main />} />
-        <Route path="/test" element={<TestPage />}/>
+        <Route path="/test" element={<TestPage />} />
         <Route path="*" element={<NotFound />} />
+        <Route path="/admin/account" element={<Account />} />
       </Routes>
     </BrowserRouter>
   </RecoilRoot>
