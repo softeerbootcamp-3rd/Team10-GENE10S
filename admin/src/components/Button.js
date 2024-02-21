173b714a81ad7cee8e3894a23f1e68511f849016
h-sooah <s2sooas2@ewhain.net>
2024-02-21 13:53:37 +0900

    refactor : BtnBlack 컴포넌트에 onClick 속성 추가

--- admin/src/components/Button.js
+++ admin/src/components/Button.js
@@ -1,8 +1,8 @@
 import classNames from "classnames";
 
-export default function BtnDark({ text }) {
+export default function BtnDark({ text, onClick }) {
     return (
-        <div className={classNames('btn-dark')}>
+        <div className={classNames('btn-dark')} onClick={onClick}>
             <span>{text}</span>
         </div>
     );
