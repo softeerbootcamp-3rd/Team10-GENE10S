e96fefafb3d0679be8f2900a2cbaaf9eef946f3f
admin <rlawnghks99@gmail.com>
2024-02-21 12:03:27 +0900

    feat: 관리자 로그인 페이지 구성 및 스타일링

--- admin/src/pages/Main.js
+++ admin/src/pages/Main.js
@@ -1,7 +1,39 @@
 import classNames from 'classnames';
+import BtnDark from '../components/Button';
 
 export default function Main() {
   return (
-    <div className={classNames('content')}>Main</div>
+    <div className={classNames('login-page')}>
+      <div className={classNames('login-frame')}>
+        <div className={classNames('title')}>
+          <div className={classNames('text')}>
+            Genesis Airport
+          </div>
+          <div className={classNames('text')}>
+            Admin
+          </div>
+        </div>
+
+        <div className={classNames('body')}>
+          <div className={classNames('content')}>
+            <div className={classNames('input-container')}>
+              <div className={classNames('text')}>
+                아이디
+              </div>
+              <input type='text' className={classNames('input-area')}/>
+            </div>
+            <div className={classNames('input-container')}>
+              <div className={classNames('text')}>
+                비밀번호
+              </div>
+              <input type='password' className={classNames('input-area')}/>
+            </div>
+            <div className={classNames('btn-dark')}>
+              <BtnDark text='로그인'/>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
