8094b35d321b25ab3071e121703e9b04b0abd778
LEE, TAEGON <102223636+taegon98@users.noreply.github.com>
2024-02-22 14:00:56 +0900

    hotfix: GlobalExceptionHandler 응답 코드 변경
    - Http 응답 코드를 무조건적으로 200을 반환하고, data로 감싸진 부분에서 스프링부트 서버에서 발생한 응답코드를 보내도록 수정

--- server/src/main/java/com/genesisairport/reservation/common/util/GlobalExceptionHandler.java
+++ server/src/main/java/com/genesisairport/reservation/common/util/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import com.genesisairport.reservation.common.exception.GeneralException;
 import com.genesisairport.reservation.common.model.DataResponseDto;
 import com.genesisairport.reservation.common.enums.ResponseCode;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,7 +17,6 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(GeneralException.class)
     public ResponseEntity<Object> handleGeneralException(GeneralException generalException, WebRequest webRequest){
-
         return handleExceptionInternal(
                 generalException,
                 generalException.getResponseCode(),
@@ -24,8 +24,18 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
                 webRequest);
     }
 
+    @ExceptionHandler
+    public ResponseEntity<Object> exception(Exception e, WebRequest request) {
+        return handleExceptionInternal(
+                e,
+                ResponseCode.INTERNAL_ERROR,
+                HttpHeaders.EMPTY,
+                request
+        );
+    }
+
     private ResponseEntity<Object> handleExceptionInternal(Exception e, ResponseCode responseCode,
-                                                           HttpHeaders headers, WebRequest webRequest) {
+                                                           HttpHeaders headers, WebRequest webRequest){
 
         DataResponseDto<Object> body = DataResponseDto.of(null, responseCode.getMessage(e));
 
@@ -33,7 +43,7 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
                 e,
                 body,
                 headers,
-                responseCode.getHttpStatus(),
+                HttpStatus.OK,
                 webRequest);
     }
 }
