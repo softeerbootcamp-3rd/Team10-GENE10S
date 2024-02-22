d59f2380cf96ad2bbcce6e883ef0a68252ba1454
LEE, TAEGON <taegon1998@gmail.com>
2024-02-22 10:59:29 +0900

    remove: TestPage.js 삭제

--- admin/src/index.js
+++ admin/src/index.js
@@ -5,7 +5,6 @@ import "./styles/styles.scss";
 import Main from "./pages/Main";
 import NotFound from "./pages/NotFound";
 import { RecoilRoot } from "recoil";
-import TestPage from "./pages/TestPage";
 import ReservationDetail from "./pages/ReservationDetail";
 import ReservationList from "./pages/ReservationList";
 import Account from "./pages/Account";
@@ -17,7 +16,6 @@ root.render(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Main />} />
-        <Route path="/test" element={<TestPage />} />
         <Route path="reservation" element={<ReservationList />} />
         <Route path="/reservation/detail" element={<ReservationDetail />} />
         <Route path="*" element={<NotFound />} />
