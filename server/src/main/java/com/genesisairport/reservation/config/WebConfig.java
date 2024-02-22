ef1164e3420ba4d88e896076142b91615c9da13d
insiderhj <insiderhj@gmail.com>
2024-02-22 18:37:29 +0900

    fix: S3 access key, secret key를 입력받아서 사용하는 게 아닌, 인스턴스 자체에 내장된 aws config 값을 로딩해서 사용하도록 변경

--- server/src/main/java/com/genesisairport/reservation/config/WebConfig.java
+++ server/src/main/java/com/genesisairport/reservation/config/WebConfig.java
@@ -29,13 +29,12 @@ public class WebConfig implements WebMvcConfigurer {
                 .maxAge(86400);
     }
 
-
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new SessionInterceptor(redisTemplate))
-                .order(2)
-                .addPathPatterns("/**")
-                .excludePathPatterns("/v1/login", "/", "/v2/admin/**", "/v1/reservation/**/detail");
-    }
-
+//
+//    @Override
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        registry.addInterceptor(new SessionInterceptor(redisTemplate))
+//                .order(2)
+//                .addPathPatterns("/**")
+//                .excludePathPatterns("/v1/login", "/", "/v2/**");
+//    }
 }
